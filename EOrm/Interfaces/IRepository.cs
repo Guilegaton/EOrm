e6cf9eb473750d1f0dfd1170450f1a42047808b5
Julius <yulii.vedkal@nure.ua>
2021-05-19 20:19:48 +0300

    [add] - repository generic interface

---
+++ EOrm/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace EOrm.Interfaces
+{
+    public interface IRepository<TModel> where TModel : class, new()
+    {
+
+        void Create(TModel model);
+        void DeleteById(int id);
+        IEnumerable<TModel> GetAll();
+        TModel GetById(int id);
+        void UpdateById(TModel model);
+
+    }
+}
