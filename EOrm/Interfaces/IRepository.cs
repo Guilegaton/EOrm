e82207b75634e99d32bb661b44f988ebb14595e7
Julius <yulii.vedkal@nure.ua>
2021-05-19 20:21:33 +0300

    [update] - add missed methods to repository interface

--- EOrm/Interfaces/IRepository.cs
+++ EOrm/Interfaces/IRepository.cs
@@ -4,12 +4,18 @@ namespace EOrm.Interfaces
 {
     public interface IRepository<TModel> where TModel : class, new()
     {
+        void CommitChanges();
 
         void Create(TModel model);
-        void DeleteById(int id);
-        IEnumerable<TModel> GetAll();
-        TModel GetById(int id);
-        void UpdateById(TModel model);
 
+        void DeleteById(int id);
+
+        void DiscardChandes();
+
+        IEnumerable<TModel> GetAll();
+
+        TModel GetById(int id);
+
+        void UpdateById(TModel model);
     }
 }
