6acbd2066e5491082902ac0e536ab880b0af4b3d
Julius <yulii.vedkal@nure.ua>
2021-05-22 17:56:24 +0300

    [add] - custom sql table attributes

---
+++ EOrm/Attributes/PrimaryKeyAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EOrm.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class PrimaryKeyAttribute : Attribute
+    {
+    }
+}
